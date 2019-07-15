795b29bc12be9d261b771a64402cb7ca481476b1
nferraro <ni.ferraro@gmail.com>
2019-07-15 15:33:27 +0200

    Initial project scaffolding

---
+++ pkg/apis/yaks/v1alpha1/test_types.go
@@ -0,0 +1,50 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// TestSpec defines the desired state of Test
+// +k8s:openapi-gen=true
+type TestSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// TestStatus defines the observed state of Test
+// +k8s:openapi-gen=true
+type TestStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Test is the Schema for the tests API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+type Test struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   TestSpec   `json:"spec,omitempty"`
+	Status TestStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// TestList contains a list of Test
+type TestList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Test `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Test{}, &TestList{})
+}
---
+++ pkg/controller/add_test.go
@@ -0,0 +1,10 @@
+package controller
+
+import (
+	"github.com/jboss-fuse/yaks/pkg/controller/test"
+)
+
+func init() {
+	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
+	AddToManagerFuncs = append(AddToManagerFuncs, test.Add)
+}
