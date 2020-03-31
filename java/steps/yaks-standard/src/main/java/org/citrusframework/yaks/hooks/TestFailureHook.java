9595863ff9e622daa2b5ff14d22dd3864cabb6c3
Christoph Deppisch <cdeppisch@redhat.com>
2020-03-31 10:50:13 +0200

    fix: False positive Citrus test reports
    Align failure state in Cucumber and Citrus. When failure is outside of the Citrus test action scope (e.g. ActiveMQ connection error) the Citrus test reports were successful. Now make sure to also add the error to the Citrus reporter in that case.

---
+++ java/steps/yaks-standard/src/main/java/org/citrusframework/yaks/hooks/TestFailureHook.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.citrusframework.yaks.hooks;
+
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.actions.AbstractTestAction;
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.context.TestContext;
+import com.consol.citrus.exceptions.CitrusRuntimeException;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+
+/**
+ * Failure hook makes sure that the Citrus test state is set to FAILED when the Cucumber scenario is failed. This is because
+ * the error might not be within the Citrus test actions but in some general Cucumber step (e.g. when connection to JMS message broker failed).
+ *
+ * This would then lead to a false positive Citrus test report because the error was not part of the Citrus test. This hook makes sure to always set
+ * an exception on the test context in order to correctly reflect the scenario failed state.
+ *
+ * @author Christoph Deppisch
+ */
+public class TestFailureHook {
+
+    @CitrusResource
+    private TestCaseRunner runner;
+
+    @After(order = Integer.MAX_VALUE)
+    public void checkTestFailure(Scenario scenario) {
+        if (scenario.isFailed()) {
+            runner.run(new AbstractTestAction() {
+                @Override
+                public void doExecute(TestContext context) {
+                    context.getExceptions().add(new CitrusRuntimeException(
+                            String.format("Scenario %s:%s status %s", scenario.getId(), scenario.getLine(), scenario.getStatus().toString())));
+                }
+            });
+        }
+    }
+}
