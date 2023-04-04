e6eea0d966936672c05c2797ad449a5190998902
Christoph Deppisch <cdeppisch@redhat.com>
2023-04-04 21:16:34 +0200

    chore: Fix Kamelet apiVersion EnvVar settings

--- java/steps/yaks-camel-k/src/main/java/org/citrusframework/yaks/camelk/CamelKSettings.java
+++ java/steps/yaks-camel-k/src/main/java/org/citrusframework/yaks/camelk/CamelKSettings.java
@@ -59,11 +59,11 @@ public final class CamelKSettings {
     public static final String API_VERSION_DEFAULT = V1;
 
     private static final String KAMELET_API_VERSION_PROPERTY = CAMELK_PROPERTY_PREFIX + "kamelet.api.version";
-    private static final String KAMELET_API_VERSION_ENV = CAMELK_ENV_PREFIX + "_KAMELET_API_VERSION";
+    private static final String KAMELET_API_VERSION_ENV = CAMELK_ENV_PREFIX + "KAMELET_API_VERSION";
     public static final String KAMELET_API_VERSION_DEFAULT = V1ALPHA1;
 
     private static final String KAMELET_BINDING_API_VERSION_PROPERTY = CAMELK_PROPERTY_PREFIX + "kamelet.binding.api.version";
-    private static final String KAMELET_BINDING_API_VERSION_ENV = CAMELK_ENV_PREFIX + "_KAMELET_BINDING_API_VERSION";
+    private static final String KAMELET_BINDING_API_VERSION_ENV = CAMELK_ENV_PREFIX + "KAMELET_BINDING_API_VERSION";
 
     private static final String AUTO_REMOVE_RESOURCES_PROPERTY = CAMELK_PROPERTY_PREFIX + "auto.remove.resources";
     private static final String AUTO_REMOVE_RESOURCES_ENV = CAMELK_ENV_PREFIX + "AUTO_REMOVE_RESOURCES";
