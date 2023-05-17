de27dd01ab2f62d906c3326439c03d84364ba46f
Christoph Deppisch <cdeppisch@redhat.com>
2023-05-17 20:35:33 +0200

    chore: Add OLM CSV v0.15.1

--- java/tools/yaks-jbang/dist/YaksJBang.java
+++ java/tools/yaks-jbang/dist/YaksJBang.java
@@ -19,9 +19,9 @@
 
 //JAVA 11+
 //REPOS mavencentral
-//DEPS org.citrusframework.yaks:yaks-parent:${yaks.jbang.version:0.15.0}@pom
-//DEPS org.citrusframework.yaks:yaks-jbang:${yaks.jbang.version:0.15.0}
-//DEPS org.citrusframework.yaks:yaks-runtime-core:${yaks.jbang.version:0.15.0}
+//DEPS org.citrusframework.yaks:yaks-parent:${yaks.jbang.version:0.15.1}@pom
+//DEPS org.citrusframework.yaks:yaks-jbang:${yaks.jbang.version:0.15.1}
+//DEPS org.citrusframework.yaks:yaks-runtime-core:${yaks.jbang.version:0.15.1}
 package main;
 
 import org.citrusframework.yaks.jbang.YaksJBangMain;
