0efb980bbce20a9ae6445b9ad0ac01994f63d75d
Christoph Deppisch <cdeppisch@redhat.com>
2024-05-17 14:50:13 +0200

    chore: Update to version 0.19.2 in YAKS JBang dist

--- java/tools/yaks-jbang/dist/YaksJBang.java
+++ java/tools/yaks-jbang/dist/YaksJBang.java
@@ -18,10 +18,10 @@
 
 //JAVA 17+
 //REPOS mavencentral
-//DEPS org.citrusframework.yaks:yaks-parent:${yaks.jbang.version:0.19.0}@pom
-//DEPS org.citrusframework.yaks:yaks-jbang:${yaks.jbang.version:0.19.0}
-//DEPS org.citrusframework.yaks:yaks-runtime-core:${yaks.jbang.version:0.19.0}
-//DEPS org.citrusframework:citrus-jbang:${citrus.jbang.version:4.2.0}
+//DEPS org.citrusframework.yaks:yaks-parent:${yaks.jbang.version:0.19.2}@pom
+//DEPS org.citrusframework.yaks:yaks-jbang:${yaks.jbang.version:0.19.2}
+//DEPS org.citrusframework.yaks:yaks-runtime-core:${yaks.jbang.version:0.19.2}
+//DEPS org.citrusframework:citrus-jbang:${citrus.jbang.version:4.2.1}
 package main;
 
 import org.citrusframework.yaks.jbang.YaksJBangMain;
